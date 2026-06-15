1aedec27cf93794f15cee2041d2af164b165401c
kovalchuk412 <33413023+kovalchuk412@users.noreply.github.com>
2026-06-15 17:38:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-// URL бекенду. Локально: http://localhost:3000
-// Після деплою заміни на адресу Render/Railway/Fly, наприклад:
-// window.API_BASE = "https://esoz-board-api.onrender.com";
-window.API_BASE = "http://localhost:3000";
+// URL бекенду
+window.API_BASE = "https://esoz-hypothesis-board-api.onrender.com";
