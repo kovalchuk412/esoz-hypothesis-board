bc9aa4d27c300501c944199cf0d0e870e9d8776a
kovalchuk412 <33413023+kovalchuk412@users.noreply.github.com>
2026-06-15 17:09:40 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// URL бекенду. Локально: http://localhost:3000
+// Після деплою заміни на адресу Render/Railway/Fly, наприклад:
+// window.API_BASE = "https://esoz-board-api.onrender.com";
+window.API_BASE = "http://localhost:3000";
